33ad3c511b392ca3c96bee70dcdbd036249cf6bd
johnmiko01 <solomonjohnmiko14@gmail.com>
2021-09-09 14:36:49 +0800

    init commit

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -6,22 +6,19 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private static final long serialVersionUID = 1L;
 
-    public void init() {
-        message = "Hello World!";
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doPost(request, response);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h1>Hello World!.</h1>");
+        }
 
-    public void destroy() {
+
     }
 }
